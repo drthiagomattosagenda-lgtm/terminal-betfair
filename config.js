87109d9ca663bbfdd39ce946bb74f2bab939bb7c
drthiagomattosagenda-lgtm <dr.thiagomattos.agenda@gmail.com>
2026-04-02 10:43:37 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+const CONFIG = {
+  // Cole aqui a chave que você recebeu por e-mail
+  API_KEY: "f0772d6c4ebf4102b615b3466f97fd4b",
+  
+  // Ligas que você quer exibir (IDs do football-data.org)
+  LIGAS: {
+    2013: "Brasileirão Série A",
+    2021: "Premier League",
+    2014: "La Liga",
+    2001: "Champions League",
+    2002: "Bundesliga",
+    2019: "Serie A"
+  }
+};
